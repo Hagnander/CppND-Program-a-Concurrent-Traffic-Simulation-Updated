e77d651047958c2df0576dc0069092f68f0255e2
Hagnander <johan.hagnander@movinity.ai>
2021-01-20 13:21:18 +0100

    All tasks completed

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -3,38 +3,25 @@
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
-
- 
 template <typename T>
 T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
-    
+{    
     // perform queue modification under the lock
     std::unique_lock<std::mutex> uLock(_mutex);
     _condition.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
-
     // remove last vector element from queue
     T msg = std::move(_queue.back());
     _queue.pop_back();
-
     return msg; // will not be copied due to return value optimization (RVO) in C++
-    
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     // simulate some work
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
     // perform vector modification under the lock
     std::lock_guard<std::mutex> uLock(_mutex);
-
     // add vector to queue
     _queue.push_back(std::move(msg));
     _condition.notify_one(); // notify client after pushing new Vehicle into vector
@@ -42,21 +29,16 @@ void MessageQueue<T>::send(T &&msg)
 
 
 /* Implementation of class "TrafficLight" */
-
-
 TrafficLight::TrafficLight()
 {
-    _currentPhase = TrafficLightPhase::red;
+    _currentPhase = TrafficLightPhase::kRed;
 }
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
     while (true)
     {
-        if (_queue.receive() == green)
+        if (_queue.receive() == kGreen)
             return;
     }
 }
@@ -68,35 +50,28 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
     threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
     std::chrono::time_point<std::chrono::steady_clock> TimeSinceLastToggle;
     double TimeToNextToggle;
-    TimeSinceLastToggle = std::chrono::steady_clock::now();
-    TimeToNextToggle = rand() % 3 + 4; 
-    //std::cout<<"In cycleThroug..."<<_TimeToNextToggle<<std::endl;
+    TimeSinceLastToggle = std::chrono::steady_clock::now(); //Set init value
+    TimeToNextToggle = rand() % 3 + 4; //Time to next toggle should be a random value in the range of 4-6
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        auto t1 = std::chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = t1 - TimeSinceLastToggle;
-        if (elapsed_seconds.count() >=  TimeToNextToggle)
+        auto CurrentTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> Elapsed_Time = CurrentTime - TimeSinceLastToggle;
+        if (Elapsed_Time.count() >=  TimeToNextToggle)
         {
-            //std::cout<<_TimeToNextToggle<<std::endl;
-            if (_currentPhase == red)
-                _currentPhase = green;
+            if (_currentPhase == kRed)
+                _currentPhase = kGreen;
             else
-                _currentPhase = red;
-            TimeSinceLastToggle = t1;
+                _currentPhase = kRed;
+            TimeSinceLastToggle = CurrentTime;
             TimeToNextToggle = rand() % 3 + 4;            
            _queue.send(std::move(_currentPhase));
         }
